91f01d93d82d0084a330aaa64dfcc67635047b98
Nick <nikitalytvynov0506@gmail.com>
2023-11-10 18:09:38 +0200

    sii

---
+++ src/components/Skeleton.js
@@ -0,0 +1,14 @@
+import classNames from "classnames";
+
+function Skeleton({ times }) {
+    const boxes = Array(times).fill(0).map((_, i) => {
+        return <div key={i}></div>;
+    });
+
+    return boxes;
+    // for (let i = 0; i < times; i++) {
+    //     boxes.push(<div >)
+    // }
+}
+
+export default Skeleton;
--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
 
 const usersSlice = createSlice({
     name: 'users',
@@ -6,10 +7,22 @@ const usersSlice = createSlice({
         data: [],
         isLoading: false,
         error: null
-
     },
-    reducers: {}
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.data = action.payload;
+        });
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
+        });
+    }
 });
 
 export const usersReducer = usersSlice.reducer;
 
+ 
